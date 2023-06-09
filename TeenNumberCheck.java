8bf44a5cefe668470467682387936e6e47b5fa00
Ajithhh <ajithvn01@gmail.com>
2023-06-09 10:07:49 +0300

    aa

--- TeenNumberCheck.java
+++ TeenNumberCheck.java
@@ -1,4 +1,4 @@
-public class TeenNumberChecker {
+public class TeenNumberCheck {
     public static boolean hasTeen(int firstNumber , int secondNumber, int thirdNumber){
         if( firstNumber>=13 &&  firstNumber<=19 || secondNumber>=13 && secondNumber<=19 || thirdNumber>=13 && thirdNumber<=19 ){
             return true;
